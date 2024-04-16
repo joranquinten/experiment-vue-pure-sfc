a4a236a0b6aec04846fcf1d45f8ee5c0b351cff5
Joran Quinten <joran@joranquinten.nl>
2024-04-16 15:46:58 +0200

    feat: enable in-source compoent testing

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -47,6 +47,85 @@ const Counter = defineComponent({
   },
 });
 export default Counter;
+
+import { shallowMount } from "@vue/test-utils";
+if (import.meta.vitest) {
+  const { it, expect, describe } = import.meta.vitest;
+  describe("Counter Pure SFC", (): void => {
+    it("renders the initial count correctly", (): void => {
+      const wrapper = shallowMount(Counter);
+      expect(wrapper.find('[data-testid="count"]').text()).toBe("0");
+    });
+
+    it("increments the count when the increase button is clicked", async (): Promise<void> => {
+      const wrapper = shallowMount(Counter);
+      const increaseButton = wrapper.find('[data-testid="increase"]');
+      await increaseButton.trigger("click");
+      expect(wrapper.find('[data-testid="count"]').text()).toBe("1");
+    });
+
+    it("decrements the count when the decrease button is clicked and count is bigger than the min", async (): Promise<void> => {
+      const wrapper = shallowMount(Counter);
+      const decreaseButton = wrapper.find('[data-testid="decrease"]');
+      await decreaseButton.trigger("click");
+      expect(wrapper.find('[data-testid="count"]').text()).toBe("0");
+    });
+
+    it("decrements the count when the decrease button is clicked", async (): Promise<void> => {
+      const wrapper = shallowMount(Counter, { props: { min: -10 } });
+      const decreaseButton = wrapper.find('[data-testid="decrease"]');
+      await decreaseButton.trigger("click");
+      expect(wrapper.find('[data-testid="count"]').text()).toBe("-1");
+    });
+
+    it("resets the count when the reset button is clicked", async (): Promise<void> => {
+      const wrapper = shallowMount(Counter);
+      const increaseButton = wrapper.find('[data-testid="increase"]');
+      const resetButton = wrapper.find('[data-testid="reset"]');
+      await increaseButton.trigger("click");
+      await resetButton.trigger("click");
+      expect(wrapper.find('[data-testid="count"]').text()).toBe("0");
+    });
+
+    it("disables the increase button when the count reaches the max value", async (): Promise<void> => {
+      const wrapper = shallowMount(Counter, {
+        props: {
+          max: 2,
+        },
+      });
+      const increaseButton = wrapper.find('[data-testid="increase"]');
+      await increaseButton.trigger("click");
+      await increaseButton.trigger("click");
+      expect(increaseButton.attributes()).toMatchObject({ disabled: "" });
+    });
+
+    it("disables the decrease button when the count reaches the min value", async (): Promise<void> => {
+      const wrapper = shallowMount(Counter, {
+        props: {
+          min: -2,
+        },
+      });
+      const decreaseButton = wrapper.find('[data-testid="decrease"]');
+      await decreaseButton.trigger("click");
+      await decreaseButton.trigger("click");
+      expect(decreaseButton.attributes()).toMatchObject({ disabled: "" });
+    });
+
+    it("disables the reset button when the count is not dirty", (): void => {
+      const wrapper = shallowMount(Counter);
+      const resetButton = wrapper.find('[data-testid="reset"]');
+      expect(resetButton.attributes()).toMatchObject({ disabled: "" });
+    });
+
+    it("enables the reset button when the count is dirty", async (): Promise<void> => {
+      const wrapper = shallowMount(Counter);
+      const increaseButton = wrapper.find('[data-testid="increase"]');
+      const resetButton = wrapper.find('[data-testid="reset"]');
+      await increaseButton.trigger("click");
+      expect(resetButton.attributes()).not.toMatchObject({ disabled: "" });
+    });
+  });
+}
 </script>
 
 <template>
--- vitest.config.ts
+++ vitest.config.ts
@@ -5,5 +5,6 @@ export default mergeConfig(viteConfig, defineConfig({
     test: {
         globals: true,
         environment: 'happy-dom',
+        includeSource: ['src/components/**/*.vue'], 
     },
 }))
